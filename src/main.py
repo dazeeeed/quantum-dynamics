2719eafc939dab3ed3fd5a2d4a7bb0a281ceb820
Chris <dazeeeed@gmail.com>
2021-11-17 21:27:47 +0100

    data plot

--- src/main.py
+++ src/main.py
@@ -8,8 +8,8 @@ import pandas as pd
 # =======================
 N = 100         # number of ranges
 n = 1           # which energy level
-kappa = 0   
-omega = 0
+kappa = 0
+omega = 0 # 3 * np.pi**2 / 2
 tau = 0
 delta_tau = 0.0001
 # =======================
@@ -27,14 +27,16 @@ H_real = np.zeros(shape=x_k.shape)
 H_imag = np.zeros(shape=x_k.shape)
 
 
-@jit()
+@jit
 def calc_H(psi, time):
     H = np.zeros(shape=x_k.shape)
-    for k in range(101):
-        if k < 1 or k >= 99:
+    for k in range(N+1):
+        if k < 1 or k >= N:
             continue
         H[k] = -0.5 / (delta_x ** 2) * (psi[k + 1] + psi[k - 1] - 2 * psi[k]) + \
                     + kappa * (x_k[k] - 0.5) * psi[k] * np.sin(omega * time)
+    H[0] = 0
+    H[-1] = 0
     return H
 
 
@@ -54,24 +56,24 @@ def make_calculations(psi_real, psi_imag, tau):
     return psi_real, psi_imag
 
 
-def write_params_to_file(rho):
-    with open("rho.txt", 'a') as f:
-        pd.DataFrame(rho).transpose(copy=True).to_csv(f, sep=',', index=False, header=None, line_terminator='\n')
+# def write_rho_to_file(rho):
+#     with open(os.path.join("..", "data", "rho.txt"), 'a') as f:
+#         pd.DataFrame(rho).transpose(copy=True).to_csv(f, sep=';', index=False, header=None, line_terminator='\n')
+def write_rho_to_file(rho):
+    with open(os.path.join("..", "data", "rho.txt"), 'a') as f:
+        pd.DataFrame(rho).transpose(copy=True).to_csv(f, sep=';', index=False, header=None, line_terminator='\n')
+
+
+def write_params_to_file(norm, x, epsilon, write_headers=False):
+    with open(os.path.join("..", "data", "temp_values.txt"), 'a') as f:
+        if write_headers:
+            f.write("norm;x;epsilon\n")
+        f.write("{};{};{}\n".format(norm, x, epsilon))
+
 
 
 def main():
-    # =======================
-    N = 100         # number of ranges
-    n = 1           # which energy level
-    kappa = 0   
-    omega = 0
     tau = 0
-    delta_tau = 0.0001
-    # =======================
-
-    delta_x = 1 / N
-    k_range = np.arange(0, 101)
-    x_k = np.linspace(0, 1, N + 1)
 
     psi_real = np.sqrt(2) * np.sin(n * np.pi * x_k)
     psi_imag = np.zeros(shape=x_k.shape)
@@ -79,24 +81,32 @@ def main():
     H_real = np.zeros(shape=x_k.shape)
     H_imag = np.zeros(shape=x_k.shape)
 
-    NUM_STEPS = 200
+    rho_arr = np.zeros(shape=(2,x_k.shape[0]))
+
+    write_headers = True
+    NUM_STEPS = 4000
     for step in tqdm(range(NUM_STEPS)):
         H_real = calc_H(psi_real, tau)
         H_imag = calc_H(psi_imag, tau)
 
-        if step % 2 == 0:
-            norm = delta_x * np.sum(psi_real**2 + psi_imag**2)
-            x = delta_x * np.sum(x_k * (psi_real**2 + psi_imag**2))
-            epsilon = delta_x * np.sum(psi_real * H_real + psi_imag * H_imag)
+        # if step % 2 == 0:
+        #     norm = delta_x * np.sum(psi_real**2 + psi_imag**2)
+        #     x = delta_x * np.sum(x_k * (psi_real**2 + psi_imag**2))
+        #     epsilon = delta_x * np.sum(psi_real * H_real + psi_imag * H_imag)
 
         psi_real, psi_imag = make_calculations(psi_real, psi_imag, tau)
 
         rho = psi_real**2 + psi_imag**2
 
         if step % 10 == 0:
-            write_params_to_file(rho)
+            rho_arr = np.vstack([rho_arr, rho])
+            # write_rho_to_file(rho)
+            # write_params_to_file(norm, x, epsilon, write_headers=write_headers)
         
+        write_headers = False
         tau += delta_tau
+
+    write_rho_to_file(rho_arr)
 
 
 
@@ -104,10 +114,14 @@ if __name__ == '__main__':
     start_time = time.time()
 
     os.chdir(os.path.abspath(os.path.dirname(__file__)))
-    rho_file = os.path.join("rho.txt")
+    rho_file = os.path.join("..", "data", "rho.txt")
+    values_file = os.path.join("..", "data", "temp_values.txt")
+
 
     if os.path.exists(rho_file):
         os.remove(rho_file)
+    if os.path.exists(values_file):
+        os.remove(values_file)
 
     main()
     print("--- %s seconds ---" % (time.time() - start_time))
---
+++ src/plot.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+
+def main():
+    rho = pd.read_csv(os.path.join("..", "data", "rho.txt"), sep=';', header=None)
+    
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, 1), ylim=(0,5))
+    line, = ax.plot([], [], lw=3)
+
+    def init():
+        line.set_data([], [])
+        return line,
+
+    def animate(i):
+        x = np.linspace(0, 1, 101)
+        y = rho.iloc[i, :]
+        line.set_data(x, y)
+        return line,
+
+    anim = FuncAnimation(fig, animate, init_func=init,
+                                frames=200, interval=2, blit=True)
+
+
+    save_animation = False
+    if save_animation:
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=60, metadata=dict(artist='Me'), bitrate=-1, extra_args=['-vcodec', 'libx264'])
+        anim.save('wave_function.mp4', writer=writer)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    main()
